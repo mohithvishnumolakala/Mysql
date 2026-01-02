703fcbb6e367d5245230dac94dd1634ff32da3f8
mohith <mohithvishnumolakala@gmail.com>
2026-01-02 16:42:58 +0530

    replaced file

---
+++ view_data.sql
@@ -0,0 +1,5 @@
+select * from firsttable.student_table;
+-- i created a table named student_table in schema of firsttable and i used a command to view all columns in my table
+
+-- for specific colums view we use
+SELECT student_name,student_phoneno FROM firsttable.student_table;
